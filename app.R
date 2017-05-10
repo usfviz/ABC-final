ced0d6197019aceb1bc4764fae743a9d39c3bbe2
Valentin Vrzheshch <vv@Valentins-MacBook-Pro.local>
2017-05-10 14:42:05 -0700

    Added Bar Chart

--- app.R
+++ app.R
@@ -14,6 +14,7 @@ library(shinydashboard)
 df_sankey <- read.csv("df_sankey.csv", stringsAsFactors = FALSE)
 df_map    <- read.csv("df_map.csv",    stringsAsFactors = FALSE)
 df_tree   <- read.csv("df_tree.csv",   stringsAsFactors = FALSE)
+df_barchart   <- read.csv("df_barchart.csv",   stringsAsFactors = FALSE)
 
 non_countries <- c("World", "Extra-trade", "Other Africa", "Other Europe", "Other Asia", "Africa", "Europe", "ACP (Africa, Caribbean and Pacific Countries)", "Africa, CIS and Middle East", "Andean Community", "American Samoa", "APEC (Asia-Pacific Economic Cooperation)", "Aruba (the Netherlands with respect to)", "Asia", "Asia excluding Hong Kong re-exports", "Asia less JPN,ANZ,CHN,NICS4, IN", "Australia and New Zealand", "BRIC members", "BRICS members", "CACM (Central American Common Market)", "CARICOM (Caribbean Community)",  "CEMAC (Economic and Monetary Community of Central Africa)", "Chinese Taipei", "COMESA (Common Market for Eastern and Southern Africa)", "Commonwealth of Independent States (CIS)", "Czech and Slovak Fed. Rep., former", "Developing and Emerging Economies", "ECCAS (Economic Community of Central African States)", "ECOWAS (Economic Community of West African States)", "EFTA (European Free Trade Association)", "Europe (Indices only)", "Europe excluding EU(28) intra-trade", "European Union (12)", "European Union (15)", "European Union (27)", "European Union (28)", "Four East Asian traders", "French Southern and Antarctic Territory", "French Guiana", "French Polynesia", "G20 - Developed Economies", "G20 - Developed Economies excl EU", "G20 - Developing economies", "G20 Members", "G20 Members exc. EU incl.FR, DE,IT,UK", "GCC (Gulf Co-operation Council)", "German Dem. Rep., former", "LDC (Least developed countries)", "LDC exporters of agriculture", "LDC exporters of manufactures", "LDC oil exporters", "MERCOSUR (Southern Common Market)", "Middle East", "NAFTA (North American Free Trade Agreement)", "Netherlands Antilles", "North America", "SADC (Southern African Development Community)", "SAFTA (South Asian Free Trade Agreement)", "South Africa", "South America excluding Brazil", "South and Central America", "Switzerland (Excl. Gold)", "WAEMU (West African Economic and Monetary Union)", "World", "World (only indices- excluding HK RX and CH Gold)", "World excluding EU(28) intra-trade", "WTO Members 2015", "WTO Members 2015 Incl. HK RX", "Developing Asia excluding Hong Kong re-exports", "ASEAN (Association of South East Asian Nations)",'Other CIS (CIS11)')
 non_countries_2 <- paste0(non_countries,' ')
@@ -33,6 +34,11 @@ ui <- dashboardPage(
                          selectInput("ind.globe", "Indicator:", unique(df_map$Indicator_description))
           ),
         menuItem("Tree Map", tabName = "treemap", icon = icon("columns")),
+        menuItem("Bar Chart", tabName = "barchart", icon = icon("bar-chart")),
+        conditionalPanel("input.sidebarmenu === 'barchart'",
+                         sliderInput("slider.barchart", "Top:", min=1, max=length(unique(df_barchart$Reporter_description)), value=10),
+                         selectInput("ind.bar.chart", "Indicator:", unique(df_map$Indicator_description))
+                        ),
         sliderInput("year", "Year", min = min(df_map$Year), max = max(df_map$Year), value = max(df_map$Year)-4, animate = TRUE, step = 1, sep = "")
       )
     ),
@@ -40,7 +46,8 @@ ui <- dashboardPage(
       tabItems(
         tabItem(tabName = "sankey",  htmlOutput("sankey.plot")),
         tabItem(tabName = "treemap", d3tree2Output("d3tree",height=700, width=950)),
-        tabItem(tabName = "globe",   htmlOutput("map"))
+        tabItem(tabName = "globe",   htmlOutput("map")),
+        tabItem(tabName = "barchart",   htmlOutput("barchart"))
       )
     )
   )
@@ -50,6 +57,12 @@ server <- function(input, output) {
   sub_df_map <- reactive({ df_map[df_map$Year == input$year &
                                   df_map$Flow_Description == input$radio.globe &
                                   df_map$Indicator_description == input$ind.globe, ]})
+  
+  sub_df_barchart <- reactive({
+    df_2 <- df_barchart[df_barchart$Year == input$year &
+                        df_barchart$Indicator_description == input$ind.bar.chart, ]
+    na.omit(df_2[1:min(input$slider.barchart, nrow(df_2)), ])
+  })
   
   sub_df_tree <- reactive({df_tree[df_tree$Year == input$year, ]})
 
@@ -88,6 +101,11 @@ server <- function(input, output) {
                options = list(height = 600, width = 900,
                               sankey="{link:{color:{fill:'lightblue', stroke: 'black', strokeWidth: .1}}}")
               )
+    })
+  
+  output$barchart <- renderGvis({
+    gvisBarChart(sub_df_barchart(), xvar = "Reporter_description", yvar = c("Exports", "Imports"),
+                 options = list( height = 900))
     })
 
   output$d3tree <- renderD3tree2({
